1f4bcbad70b9843aa7392d145244c205c0f06fd1
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2020-11-05 23:10:18 +0530

    added app2 interactive version

--- source.py
+++ source.py
@@ -3,12 +3,14 @@ from werkzeug.serving import run_simple
 
 from fileinput import app as flask_app
 from dashapps.dash1 import app as app1
+from dashapps.dash2 import app as app2
 from dashapps.dash3 import app as app3
 import sys
 
 application = DispatcherMiddleware(flask_app, {
     '/app1': app1.server,
-    '/app3' : app3.server,
+    '/app2': app2.server,
+    '/app3': app3.server,
 })
 
 if __name__ == '__main__':
